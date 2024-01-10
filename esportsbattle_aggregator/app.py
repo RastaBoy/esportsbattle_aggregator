f4013bec5f2545ef7c5cbe088c9909354d22c92a
RastaBoy <trustyone3224@gmail.com>
2024-01-10 20:12:17 +0700

    entrypoint, os.variables

--- esportsbattle_aggregator/app.py
+++ esportsbattle_aggregator/app.py
@@ -1,24 +1,15 @@
-import asyncio
 import os
 
 from loguru import logger as log
 
-from services.aggregator.esportsbattle import CS2TournamentsAggregator, FootballTournamentsAggregator
+from .services.aggregator.esportsbattle import CS2TournamentsAggregator, FootballTournamentsAggregator
 
-
-__version__ = (1,0,0,0)
-
-log.add(
-    os.path.join(os.getcwd(), 'logs', '{time:DD-MM-YYYY}.log'), 
-    format='{time:HH:mm:ss.SSSZ} | [{level}]\t| {message}'
-)
-log.critical('='*15 + ' Инициализация ESportsBattle Aggregator v'+".".join(str(x) for x in __version__) + ' ' +'='*15)
 
 
 async def start_app():
-    matches = await CS2TournamentsAggregator().aggragate()
-    print(matches)    
+    print(os.environ['TEST'])
+    print(os.environ['POSTGRES'])
+    # matches = await CS2TournamentsAggregator().aggragate()
+    # print(matches)    
     ...
 
-if __name__ == "__main__":
-    asyncio.run(start_app())
---
+++ start_app.py
@@ -0,0 +1,25 @@
+
+import os
+import dotenv
+import asyncio
+
+from loguru import logger as log
+
+from esportsbattle_aggregator.app import start_app
+
+__version__ = (1,0,0,0)
+
+log.add(
+    os.path.join(os.getcwd(), 'logs', '{time:DD-MM-YYYY}.log'), 
+    format='{time:HH:mm:ss.SSSZ} | [{level}]\t| {message}'
+)
+log.critical('='*15 + ' Инициализация ESportsBattle Aggregator v'+".".join(str(x) for x in __version__) + ' ' +'='*15)
+
+# __dotenv_path__ = os.path.join(os.getcwd(), 'test', '.env')
+__dotenv_path__ = os.path.join(os.getcwd(), '.env')
+if os.path.exists(__dotenv_path__):
+    dotenv.load_dotenv(__dotenv_path__)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_app())
