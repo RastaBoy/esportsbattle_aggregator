d6c9bb7f91fc20b4ba8218b0a586e5f34822a032
RastaBoy <trustyone3224@gmail.com>
2024-01-11 19:46:53 +0700

    config fixes, docker-compose first try

--- esportsbattle_aggregator/config.py
+++ esportsbattle_aggregator/config.py
@@ -1,12 +1,12 @@
 import os
 
 # Настройки Postgres
-POSTGRES_USERNAME=os.environ['POSTGRES_USERNAME']
-POSTGRES_PASSWORD=os.environ['POSTGRES_PASSWORD']
-POSTGRES_HOST=os.environ['POSTGRES_HOST']
-POSTGRES_PORT=os.environ['POSTGRES_PORT']
-POSTGRES_DB_NAME=os.environ['POSTGRES_DB_NAME']
+POSTGRES_USERNAME=os.environ.get('POSTGRES_USERNAME', 'esports')
+POSTGRES_PASSWORD=os.environ.get('POSTGRES_PASSWORD', 'esports')
+POSTGRES_HOST=os.environ.get('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT=os.environ.get('POSTGRES_PORT', 5432)
+POSTGRES_DB_NAME=os.environ.get('POSTGRES_DB_NAME', 'esports')
 
 # Настройки приложения
-APP_PORT=os.environ['APP_PORT']
-UPDATE_TIMEOUT=os.environ['UPDATE_TIMEOUT']
+APP_PORT=os.environ.get('APP_PORT', 11011)
+UPDATE_TIMEOUT=os.environ.get('UPDATE_TIMEOUT', 60)
